35260f4a2a9f64cc30de692d020ad12b78bb99b7
Dario Giovannetti <dev@dariogiovannetti.net>
2015-06-27 19:14:02 +0800

    Add LocalSettings.archlinux.org.php

---
+++ LocalSettings.archlinux.org.php
@@ -0,0 +1,26 @@
+<?php
+
+# Protect against web entry
+if ( !defined( 'MEDIAWIKI' ) ) {
+	exit;
+}
+
+# Use 'pretty' URLs, e.g. index.php/Page_title
+# This will likely break if PHP is set up in CGI mode, but works fine with FastCGI
+$wgUsePathInfo = true;
+
+# skin
+require_once "$IP/skins/ArchLinux/ArchLinux.php";
+$wgDefaultSkin = "archlinux";
+$wgFavicon = "{$wgStylePath}/archlinux/favicon.ico";
+
+# disabled user preferences (there may be more...)
+$wgHiddenPrefs = array(
+    "underline",
+    "justify",
+    "showtoc",
+    "editsection",
+);
+
+# uploads
+$wgEnableUploads = true;
